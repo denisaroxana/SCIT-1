6b2dd07bd940cd2206661b3738436454d6a3d4ae
Melinda G <melinda.fancsal@gmail.com>
2019-06-10 13:53:15 +0300

    test

---
+++ src/sci/inheritance/Circle.java
@@ -0,0 +1,25 @@
+package sci.inheritance;
+
+public class Circle extends Shape {
+    private final double radius;
+    final double pi = Math.PI;
+
+    public Circle() {
+        this(1);
+    }
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        // A = π r^2
+        return pi * Math.pow(radius, 2);
+    }
+
+    @Override
+    public double perimeter() {
+        // P = 2πr
+        return 2 * pi * radius;
+    }
+}
